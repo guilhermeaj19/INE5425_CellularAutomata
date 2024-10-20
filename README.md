83c3419f447d1e64d0a364aa7a393c8164503ed7
guilhermeaj19 <guilhermeadenilson16@gmail.com>
2024-10-20 18:09:10 -0300

    Editing README and errors messages

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # INE5425_CellularAutomata
 Source Code of a Cellular Automata in C++, using the terminal to print each generation of lattice
+
+## Installation
+
+To create the executable, run:
+
+```bash
+make
+```
+
+## Execution
+
+The executable needs 3 arguments (integers > 0), like seen below:
+
+```bash
+./automata <lattice_size> <number of sands> <number of iterations>
+```
--- main.cpp
+++ main.cpp
@@ -11,21 +11,21 @@ int main(int argc, char** argv) {
     srand(time(NULL));
     int lattice_size, n_sands, iterations;
     if (argc != 4) {
-        cout << "ERRO! Formato Incorreto: deve ser ./automata <lattice_size> <numero de areias> <numero de iterações>" << endl;
-        cout << "OBS: os três argumentos devem ser inteiros maior que 0" << endl;
+        cout << "ERROR! Incorrect format: must be ./automata <lattice_size> <number of sands> <number of iterations>" << endl;
+        cout << "OBS: the three arguments must be integers > 0" << endl;
         return -1;
     }
 
     try {
         lattice_size = stoi(argv[1]) + 2;
         if (lattice_size <= 0) 
-            throw std::invalid_argument("ERRO! Tipo Incorreto do <lattice_size>: deve ser um inteiro maior que 0");
+            throw std::invalid_argument("ERROR! Incorrect type of <lattice_size>: it must be a integer > 0");
         n_sands = stoi(argv[2]);
         if (n_sands <= 0) 
-            throw std::invalid_argument("ERRO! Tipo Incorreto do <número de areias>: deve ser um inteiro maior que 0");
+            throw std::invalid_argument("ERROR! Incorrect type of <number of sands>: it must be a integer > 0");
         iterations = stoi(argv[3]);
         if (iterations <= 0) 
-            throw std::invalid_argument("ERRO! Tipo Incorreto do <número de iterações>: deve ser um inteiro maior que 0");
+            throw std::invalid_argument("ERROR! Incorrect type of <number of iterations>: it must be a integer > 0");
 
     } catch(const std::invalid_argument& e) {
         cout << e.what() << endl;
